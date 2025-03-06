ad055e74e09dfac75498cc844beb1327cd5d2db1
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-06 11:03:49 +0530

    Program to convert UPPERCASE characters in a string to lowercase characters

--- ToLowercase.java
+++ ToLowercase.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-public class ToUppercase {
+public class ToLowercase {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter the string: ");
         String str = input.next();
 
-        String upperCaseString1 = getUppercase(str);
-        String upperCaseString2 = getUppercaseInbuilt(str);
+        String lowerCaseString1 = getLowercase(str);
+        String lowerCaseString2 = getLowercaseInbuilt(str);
 
-        if(upperCaseString1.equals(upperCaseString2)){
-            System.out.print("Both the functions are working fine to convert lower case to upper case: " + upperCaseString1);
+        if(lowerCaseString1.equals(lowerCaseString2)){
+            System.out.print("Both the functions are working fine to convert upper case to lower case: " + lowerCaseString1);
         }
         else{
             System.out.println("Both the strings are different, there is some logical mistake");
@@ -19,16 +19,16 @@ public class ToUppercase {
         input.close();
     }
 
-    public static String getUppercaseInbuilt(String str){
-        return str.toUpperCase();
+    public static String getLowercaseInbuilt(String str){
+        return str.toLowerCase();
     }
-    public static String getUppercase(String str){
+    public static String getLowercase(String str){
         String result = "";
         int size = str.length();
         for(int i=0;i<size;i++){
             char c = str.charAt(i);
-            if(c >= 'a' && c <= 'z'){
-                result += (char)(c-32);
+            if(c >= 'A' && c <= 'Z'){
+                result += (char)(c+32);
             }
             else result += c;
         }
