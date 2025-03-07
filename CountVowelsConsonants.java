0a4a1deaad811bda8039227f7b7340eabbf6db96
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-07 09:13:36 +0530

    Program to count vowels and consonants

--- CountVowelsConsonants.java
+++ CountVowelsConsonants.java
@@ -4,5 +4,25 @@ public class CountVowelsConsonants {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        int[] count = countVowelsConsonants(s);
+        System.out.println("Number of Vowels are: " + count[0]);
+        System.out.println("Number of Consonants are: " + count[1]);
+        input.close();
+    }
+
+    public static int[] countVowelsConsonants(String s){
+        int len = s.length();
+        int[] res = new int[2];
+        for(int i=0;i<len;i++){
+            char c = s.charAt(i);
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+                res[0]++;
+            }
+            else if(c != ' '){
+                res[1]++;
+            }
+        }
+        return res;
     }
 }
