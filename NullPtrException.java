d16976e903891a15e506a30fdb5d2385cab6ec9f
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-07 15:11:42 +0530

    Program to catch null pointer exception

---
+++ NullPtrException.java
@@ -0,0 +1,20 @@
+public class NullPtrException {
+    public static void main(String[] args) {
+        generateException();
+        handleException();
+    }
+
+    public static void generateException() {
+        String text = null;
+        System.out.println(text.length()); // This will cause a NullPointerException
+    }
+
+    public static void handleException() {
+        String text = null;
+        try {
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught: Attempted to access a method on a null object.");
+        }
+    }
+}
